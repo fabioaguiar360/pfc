8f9d336c904ba15b0885056ea59d1a0d1975d027
fabioaguiar360 <fabioaguiar360@gmail.com>
2019-06-20 09:49:06 -0300

    Until now I started the app category, create the view for this and added some categories to use it

---
+++ proj/categories/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Category
+
+# Create your views here.
+
+def categories (request):
+    categories = Category.objects.all()
+    return render(request, 'categories.html', {'categories': categories})
+
