e39c047532469bc101ae60a6d6ae0d4e0df75263
fabioaguiar360 <fabioaguiar360@gmail.com>
2019-06-21 08:45:06 -0300

    I cleaned the tables and added a foreinKey to category in INPUTS table

--- proj/inputs/models.py
+++ proj/inputs/models.py
@@ -1,9 +1,13 @@
+import sys
+sys.path.append("..")
 from django.db import models
+from categories.models import Category
 
 # Create your models here.
 class Input(models.Model):
     name        = models.CharField(max_length=250)
-    category    = models.CharField(max_length=250)
+    #category    = models.CharField(max_length=250)
+    category    = models.ForeignKey(Category, on_delete=models.CASCADE)
     value       = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __str__(self):
